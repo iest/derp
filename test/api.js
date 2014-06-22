e3ccc1fd40a89fa01ef5bc1cf5ebb63fd6b2fc81
Iestyn Williams <iest@me.com>
2014-06-22 17:27:46 +0100

    Public API tests

---
+++ test/api.js
@@ -0,0 +1,50 @@
+var should = require("should");
+var derp = require("..");
+
+describe("setup", function() {
+
+  it("should return a resolved promise", function(done) {
+    derp.setup({
+      post_directory: "test/fixtures"
+    }).then(function(stuff) {
+      stuff.should.be.ok;
+      done();
+    });
+  });
+});
+
+describe('getPost', function() {
+  beforeEach(function(done) {
+    derp.setup({
+      post_directory: "test/fixtures"
+    }).then(function() {
+      done();
+    });
+  });
+
+  it("should return the specified post", function(done) {
+    derp.getPost("derp").url.should.be.exactly("derp");
+    done();
+  });
+
+  it("should return undefined if the post doesn't exist", function(done) {
+    should.not.exist(derp.getPost('HERP'));
+    done();
+  });
+
+});
+
+describe('getAllPosts', function() {
+  beforeEach(function(done) {
+    derp.setup({
+      post_directory: "test/fixtures"
+    }).then(function() {
+      done();
+    });
+  });
+
+  it('should return an array of posts', function(done) {
+    derp.getAllPosts().should.have.lengthOf(2);
+    done();
+  });
+});
